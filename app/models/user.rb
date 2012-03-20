58ba081ed261719bfdf2b80c8a91f97fa6f6b712
Nawakkap <p.pakkawan@gmail.com>
2012-03-20 22:51:02 +0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  before_save :create_remember_token
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: 50}
@@ -10,6 +11,11 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
+  private
+
+      def create_remember_token
+        self.remember_token = SecureRandom.urlsafe_base64
+      end
 end
 
 
